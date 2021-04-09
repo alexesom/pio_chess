6de6a82e1dcd29d0fe8f473538bb0e3eeec72a55
Dominika Pacek <81358539+dominpacek@users.noreply.github.com>
2021-04-09 16:46:15 +0200

    feat: add enumerator Color, findPiece function, tryMove first version.

--- pio/src/Chessboard.java
+++ pio/src/Chessboard.java
@@ -5,15 +5,24 @@ import java.util.List;
 
 public class Chessboard {
     private static Square[][] board;
-    private List<Piece> pieces;
+    private static List<Piece> pieces;
 
     public Chessboard(List<Piece> pieces, Square[][] board) {
-        this.pieces = pieces;
+        Chessboard.pieces = pieces;
         Chessboard.board = board;
     }
 
     void createNewBoard() {
 
+    }
+
+    public static Piece findPiece(Square coordinates){
+        for (Piece i : pieces) {
+            if (i.position == coordinates) {
+                return i;
+            }
+        }
+        return null;
     }
 
 }
