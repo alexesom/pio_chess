37533a7b7964432ae361f23694515aea2f527846
Dominiola <dominiola13@gmail.com>
2021-04-09 16:46:15 +0200

    Dodano enumerator Color, funkcję findPiece, pierwsza wersja funkcji tryMove.

--- pio/src/Player.java
+++ pio/src/Player.java
@@ -17,12 +17,27 @@ public class Player {
         return false;
     }
 
-    public byte tryMove(Square oldMove, Square newMove) {
-
-        return 0;
+    public byte tryMove(Square origin, Square destination) {
+        Pieces.Piece piece = Chessboard.findPiece(origin);
+        if (piece == null){
+            return 1;
+        }
+        if (!piece.isAbleToMove()) {
+            return 2;
+        }
+        else {
+            piece.move(destination);
+            nextTurn();
+            return 0;
+        }
     }
 
     public void nextTurn() {
-
+        if (Game.current_turn == Color.WHITE){
+            Game.current_turn = Color.BLACK;
+        }
+        if (Game.current_turn == Color.BLACK){
+            Game.current_turn = Color.WHITE;
+        }
     }
 }
