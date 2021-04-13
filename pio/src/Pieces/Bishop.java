fac051cdb723b30e9fbcc78aa3cfa1e500f4d062
Paulinka-M <ktost84@gmail.com>
2021-04-13 12:53:32 +0200

    feat: add icons and implementation to show each piece on board

--- pio/src/Pieces/Bishop.java
+++ pio/src/Pieces/Bishop.java
@@ -1,8 +1,13 @@
 package Pieces;
 
 public class Bishop extends Piece implements PieceInterface{
-    public Bishop() {
+    Square buttonSquare = null;
+    Color bishopColor;
 
+    public Bishop(Square bishopPosition, Color bishopColor) {
+        this.buttonSquare = bishopPosition;
+        this.bishopColor = bishopColor;
+        super.buttonSquare = this.buttonSquare;
     }
 
     @Override
