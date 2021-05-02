d933627dd1cbc4974a3d626c3b81d37773caf7fb
alexesom <alexey.samovich@gmail.com>
2021-05-02 22:07:52 +0200

    fix: fix PieceList constructor

--- pio/src/ChessInterface/PieceList.java
+++ pio/src/ChessInterface/PieceList.java
@@ -6,9 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PieceList {
-
+    final King whiteKing;
+    final King blackKing;
     List<Piece> whitePieces = new ArrayList<Piece>();
     List<Piece> blackPieces = new ArrayList<Piece>();
+
+    public PieceList(King whiteKing, King blackKing) {
+        this.whiteKing = whiteKing;
+        this.blackKing = blackKing;
+    }
 
     public void addWhitePiece(Piece piece) {
         whitePieces.add(piece);
