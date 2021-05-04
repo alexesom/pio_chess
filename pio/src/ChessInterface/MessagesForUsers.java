dff481001d3d746ae9efa3d52ee5299dfceff4f9
aleks <aleksandra.michalska01@gmail.com>
2021-05-05 01:05:17 +0200

    fix: change in user messages

--- pio/src/ChessInterface/MessagesForUsers.java
+++ pio/src/ChessInterface/MessagesForUsers.java
@@ -3,26 +3,26 @@ package ChessInterface;
 import javax.swing.*;
 
 public class MessagesForUsers {
-    private static ImageIcon imageIcon1 = new ImageIcon("pio/message1.gif");
+    private static ImageIcon imageIcon1 = new ImageIcon("pio/message1.png");
     private static JLabel message1 = new JLabel(imageIcon1);
-    private static ImageIcon imageIcon2 = new ImageIcon("pio/message2.gif");
+    private static ImageIcon imageIcon2 = new ImageIcon("pio/message2.png");
     private static JLabel message2 = new JLabel(imageIcon2);
-    private static ImageIcon imageIcon3 = new ImageIcon("pio/message3.gif");
+    private static ImageIcon imageIcon3 = new ImageIcon("pio/message3.png");
     private static JLabel message3 = new JLabel(imageIcon3);
-    private static ImageIcon imageIcon4 = new ImageIcon("pio/message4.gif");
+    private static ImageIcon imageIcon4 = new ImageIcon("pio/message4.png");
     private static JLabel message4 = new JLabel(imageIcon4);
-    private static int delay = 1600; // Delay in milliseconds
+    private static int delay = 1500; // Delay in milliseconds
     public static JPanel messagePanel = new JPanel();
 
     private static void createMessagePanel() {
         messagePanel.setOpaque(false);
         messagePanel.setVisible(false);
+        messagePanel.setBounds(480, 640, 500, 200);
     }
 
-    public static void createMessage(JLabel messageName, int xPosition, int yPosition) {
+    public static void createMessage(JLabel messageName) {
         createMessagePanel();
 
-        messagePanel.setBounds(xPosition, yPosition, 500, 200);
         messagePanel.add(messageName);
         messagePanel.setVisible(true);
 
@@ -35,18 +35,18 @@ public class MessagesForUsers {
     }
 
     public static void createMessage1() {
-        createMessage(message1, 450, 600);
+        createMessage(message1);
     }
 
     public static void createMessage2() {
-        createMessage(message2, 550, 550);
+        createMessage(message2);
     }
 
     public static void createMessage3() {
-        createMessage(message3, 500, 600);
+        createMessage(message3);
     }
 
     public static void createMessage4() {
-        createMessage(message4, 500, 600);
+        createMessage(message4);
     }
 }
