8806d5cde64ff8b9df0ece2c16e2875e84a6256c
Dominika Pacek <81358539+dominpacek@users.noreply.github.com>
2021-05-11 18:59:37 +0200

    fix: remove duplicate test

--- pio/test/ChessboardTest.java
+++ pio/test/ChessboardTest.java
@@ -6,31 +6,18 @@ import ChessInterface.PieceList;
 import Pieces.*;
 import org.junit.jupiter.api.Test;
 
-import static ChessInterface.Chessboard.tryMove;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
-
 import java.awt.*;
+
+import static ChessInterface.Chessboard.tryMove;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ChessboardTest {
 
     @Test
-    void tryMovingPiece() {
-        Square originSquare = new Square(0,0);
-        originSquare.setSquarePiece(new Knight(originSquare, Color.WHITE));
-        Square destinationSquare = new Square (2, 1);
-        Game.current_turn = Color.WHITE;
-        try {
-            tryMove(originSquare, destinationSquare);
-            assert true;
-        } catch (Exception e) {
-            assert false;
-        }
-    }
-    @Test
     void tryMovingNullPiece() {
-        Square originSquare = new Square(0,0);
-        Square destinationSquare = new Square (1, 1);
+        Square originSquare = new Square(0, 0);
+        Square destinationSquare = new Square(1, 1);
 
         Exception thrown = assertThrows(Exception.class, () -> tryMove(originSquare, destinationSquare),
                 "expected tryMove() to throw, but it didn't");
@@ -40,9 +27,9 @@ class ChessboardTest {
 
     @Test
     void tryMovingOtherPlayersPiece() {
-        Square originSquare = new Square(0,0);
+        Square originSquare = new Square(0, 0);
         originSquare.setSquarePiece(new Pawn(originSquare, Color.BLACK));
-        Square destinationSquare = new Square (1, 1);
+        Square destinationSquare = new Square(1, 1);
         Game.current_turn = Color.WHITE;
 
         Exception thrown = assertThrows(Exception.class, () -> tryMove(originSquare, destinationSquare),
@@ -53,9 +40,9 @@ class ChessboardTest {
 
     @Test
     void tryMakingIllegalMove() {
-        Square originSquare = new Square(0,0);
+        Square originSquare = new Square(0, 0);
         originSquare.setSquarePiece(new Pawn(originSquare, Color.WHITE));
-        Square destinationSquare = new Square (1, 1);
+        Square destinationSquare = new Square(1, 1);
         Game.current_turn = Color.WHITE;
 
         Exception thrown = assertThrows(Exception.class, () -> tryMove(originSquare, destinationSquare),
@@ -66,9 +53,9 @@ class ChessboardTest {
 
     @Test
     void tryTakingOwnPiece() {
-        Square originSquare = new Square(0,0);
+        Square originSquare = new Square(0, 0);
         originSquare.setSquarePiece(new Pawn(originSquare, Color.WHITE));
-        Square destinationSquare = new Square (0, 1);
+        Square destinationSquare = new Square(0, 1);
         destinationSquare.setSquarePiece(new Piece(destinationSquare, Color.WHITE));
         Game.current_turn = Color.WHITE;
 
@@ -77,18 +64,19 @@ class ChessboardTest {
         assertTrue(thrown.getMessage().contains("tryMove exception: taking own piece"),
                 "expected tryMove() to throw taking own piece exception, but got a different one");
     }
+
     @Test
     void tryCheckingOwnKing() {
-        Square originSquare = new Square(0,1);
+        Square originSquare = new Square(0, 1);
         originSquare.setSquarePiece(new Knight(originSquare, Color.WHITE));
-        Square destinationSquare = new Square (2, 2);
+        Square destinationSquare = new Square(2, 2);
         Game.current_turn = Color.WHITE;
-        Square kingSquare = new Square(0,0);
-        PieceList.whiteKing = new King (kingSquare, Color.WHITE);
-        PieceList.whiteKing.setPieceCoordinates(0,0);
+        Square kingSquare = new Square(0, 0);
+        PieceList.whiteKing = new King(kingSquare, Color.WHITE);
+        PieceList.whiteKing.setPieceCoordinates(0, 0);
         Chessboard.board = new Square[1][1];
         Chessboard.board[0][0] = kingSquare;
-        PieceList.addListPiece(new Queen (new Square (0,2), Color.BLACK), Color.BLACK);
+        PieceList.addListPiece(new Queen(new Square(0, 2), Color.BLACK), Color.BLACK);
 
         Exception thrown = assertThrows(Exception.class, () -> tryMove(originSquare, destinationSquare),
                 "expected tryMove() to throw, but it didn't");
@@ -100,8 +88,8 @@ class ChessboardTest {
     @Test
     void tryMovingPieceInGame() {
         Game.main(null);
-        Square originSquare = Chessboard.getBoardSquare(1,1);
-        Square destinationSquare = Chessboard.getBoardSquare(1,2);
+        Square originSquare = Chessboard.getBoardSquare(1, 1);
+        Square destinationSquare = Chessboard.getBoardSquare(1, 2);
         try {
             tryMove(originSquare, destinationSquare);
             assert true;
